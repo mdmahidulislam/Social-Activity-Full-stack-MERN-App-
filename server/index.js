0e8f83525dbe853a5d431a35e12b54bf0066adde
MD. Mahidul Islam <32381876+mdmahidulislam@users.noreply.github.com>
2022-02-12 23:56:48 +0900

    component added

--- server/index.js
+++ server/index.js
@@ -3,15 +3,15 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
-
-app.use('/posts',postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://mahid:ma068093@cluster0.kkg2l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
